97f3717827fb9b5da7d5709a0d59b1628dd40f8e
Jonathan Winawer <jonathan.winawer@nyu.edu>
2014-02-16 17:38:20 -0500

    add check to see whether nifti has string std in it. if so, do not standardize it, as we assume it is already standardized

--- niftiStandardizeOrientationFromCBI.m
+++ niftiStandardizeOrientationFromCBI.m
@@ -9,7 +9,7 @@ function niftiStandardizeOrientationFromCBI(data_path)
 %
 % niftiStandardizeOrientationFromCBI(data_path)
 %
-% Example: 
+% Example:
 %   data_path = '/Volumes/server/Projects/Gamma_BOLD/wl_subj002_fieldmaps/raw';
 %   niftiStandardizeOrientationFromCBI(data_path)
 %
@@ -22,10 +22,10 @@ function niftiStandardizeOrientationFromCBI(data_path)
 xformType = 'Inplane';
 
 if ~exist(data_path, 'dir'), mkdir(data_path); end
-    
+
 %SIEMENS Field map paths
 fm_pths = matchfiles(fullfile(data_path, '*field_mapping*', '*.nii'));
-    
+
 %SIEMENS INPLANE paths
 ip_pths = matchfiles(fullfile(data_path, '*T1inplane*', '*.nii'));
 
@@ -35,31 +35,18 @@ epi_pths = matchfiles(fullfile(data_path, '*Single_Shot_epi*', '*.nii'));
 ni_pths = [ip_pths epi_pths fm_pths];
 
 %Loop through all the nifti files
-for i = 1:length(ni_pths)
+for ii = 1:length(ni_pths)
     
     % applying xform to nifti:
-    ni_pth = ni_pths{i};
-    ni = niftiRead(ni_pth);
-    ni_std = niftiApplyAndCreateXform(ni,xformType);
-
-    % look at the original and the standard nifti
-%     f = figure(101); pos = get(f, 'Position');
-%     set(f, 'Position', [pos(1) pos(2) pos(3) pos(3)*2])
-%     
-%     colormap gray; 
-%     
-%     subplot(2,1,1)
-%     imagesc(makeimagestack(mean(ni.data,4))); title('NIFTI original')
-%     axis equal tight
-%     
-%     subplot(2,1,2)
-%     imagesc(makeimagestack(mean(ni_std.data,4))); title('NIFTI standard')
-%     axis equal tight
-    
-    % write out the standard nifti    
-    ni_std.fname = strrep(ni_pth, '.nii', '_std.nii.gz');
-    disp(ni_std.fname)
-    pause(1)
-    niftiWrite(ni_std,ni_std.fname);
+    ni_pth = ni_pths{ii};
+    if isempty(strfind(ni_pth, 'rho')) && isempty(strfind(ni_pth, 'std'))
+        ni = niftiRead(ni_pth);
+        ni_std = niftiApplyAndCreateXform(ni,xformType);               
+        
+        % write out the standard nifti
+        ni_std.fname = strrep(ni_pth, '.nii', '_std.nii.gz');
+        disp(ni_std.fname)
+        niftiWrite(ni_std,ni_std.fname);
+    end
     
 end
